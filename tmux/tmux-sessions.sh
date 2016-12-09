5fe4353d7105404012588ea42d8d836acc6999ed
Alex Gaudio <adgaudio@gmail.com>
2016-12-09 18:40:35 -0500

    tweak tmux config

--- tmux/tmux-sessions.sh
+++ tmux/tmux-sessions.sh
@@ -61,7 +61,12 @@ session3print()
 sessionwork()
 {
     tmux new-session -d -s work
+    tmux new-window -k -n daemons -t work:0
+    tmux split-window -t work:daemons
+    tmux send-keys -t work:daemons.0 "cd s/ ; jupyter notebook --no-browser" Enter
+    tmux send-keys -t work:daemons.1 "cd s/ ; skype"
     tmux send-keys -t work:1 "cd s/" Enter
+    tmux select-window -t work:1
 }
 
 #
